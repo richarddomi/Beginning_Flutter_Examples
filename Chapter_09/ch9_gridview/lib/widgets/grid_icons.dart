19b48183c903a294197a90d75bea65203e22caf1
richarddomi <ricky.domi90@gmail.com>
2020-09-21 15:01:19 -0700

    Ch9_gridview

---
+++ Chapter_09/ch9_gridview/lib/widgets/grid_icons.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class GridIcons {
+  List<IconData> iconList = [];
+
+  List<IconData> getIconList() {
+    iconList
+    .. add(Icons.free_breakfast)
+    .. add(Icons.access_alarm)
+    .. add(Icons.directions_car)
+    .. add(Icons.flight)
+    .. add(Icons.cake)
+    .. add(Icons.card_giftcard)
+    .. add(Icons.face)
+    .. add(Icons.headset_mic)
+    .. add(Icons.cloud_queue)
+    .. add(Icons.exposure)
+    .. add(Icons.child_friendly)
+    .. add(Icons.edit_location)
+    .. add(Icons.lightbulb_outline)
+    .. add(Icons.lightbulb_outline);
+    return iconList;
+  }
+
+}
---
+++ Chapter_09/ch9_listview/lib/pages/home.dart
@@ -0,0 +1,26 @@
+import 'package:ch9_listview/widgets/header.dart';
+import 'package:ch9_listview/widgets/row.dart';
+import 'package:ch9_listview/widgets/row_with_card.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return HeaderWidget (index: index);
+            } else if (index >= 1 && index <= 3) {
+              return RowWithCardWidget (index: index);
+            } else {
+              return RowWidget (index: index);
+            }
+          }
+        ),
+      ),
+    );
+  }
+}
